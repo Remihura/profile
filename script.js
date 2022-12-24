754424e96ceb739f0eba868e4a34bd4918eb4a89
夜桜 <85023792+Remihura@users.noreply.github.com>
2022-12-25 01:02:42 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import $ from "https://cdn.skypack.dev/jquery@3.6.0";
-$("#na").text("宵崎 夜桜");
-$("#job").text("学生");
-$("#adcom").text("次はCSS学びたいです");
-$("#job1").text("学生");
-$("#na1").text("宵崎 叶");
-$("#com1").text("暇です");
